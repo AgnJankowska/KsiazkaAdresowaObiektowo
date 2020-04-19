f54f273d87a30cb361637c7a28fe43557ecab0fe
Agnieszka Jankowska <agn_jankowska@wp.pl>
2020-04-19 15:34:31 +0200

    Registration

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <windows.h>
 #include "Uzytkownik.h"
 using namespace std;
 
@@ -10,6 +11,12 @@ class KsiazkaAdresowa
     int idUsunietegoAdresata;
     vector <Uzytkownik> uzytkownicy;
 
+private:
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+
 public:
     void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
 };
